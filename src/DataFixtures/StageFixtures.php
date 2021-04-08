6a9d6a086db201dd1f20e6b8eb8c1aaaec28f074
Tranchant2001 <martin.guillon.perso@gmail.com>
2021-04-08 20:45:23 +0200

    Fixture faite à la main !!

--- src/DataFixtures/StageFixtures.php
+++ src/DataFixtures/StageFixtures.php
@@ -2,11 +2,13 @@
 
 namespace App\DataFixtures;
 
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Validator\Constraints\Date;
 use App\Entity\User;
 use App\Entity\Internship;
-
+use App\Entity\Category;
 
 class StageFixtures extends Fixture
 {
@@ -19,18 +21,22 @@ class StageFixtures extends Fixture
                         ->setLastName("Guillon")
                         ->setPromo(2023);
 
+            $physique = new Category();
+            $physique->setName("Physique");
+
             $stage = new Internship();
-            $stage->setAuthor("martin_guillon")
-                  ->setCategory("Physique")
+            $stage->setAuthor($utilisateur)
+                  ->setCategory($physique)
                   ->setTitle("racket de collégiens pour la levée de fonds du labo")
                   ->setDescription("<p> c'était bien lol</p>")
                   ->setCity("Marseille")
                   ->setCountry("France")
                   ->setCompany("psi_racket")
-                  ->setStartedOn(new \Date())
-                  ->setFinishedOn(new \Date());
-
+                  ->setStartedOn(new DateTime()) #erreur 'must implement interface DateTimeInterface'
+                  ->setFinishedOn(new DateTime());
+            $manager->persist($utilisateur);
             $manager->persist($stage);
+            $manager->persist($physique);
 
         $manager->flush();
     }
